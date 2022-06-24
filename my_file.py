fbb487194f9c8b267eca3e7dd0d023b7bc9208df
edoardogunelli <eg.nelli@gmail.com>
2022-06-24 10:24:11 +0200

    Reverted back to 1ce117592d63898e20f895f470d50a829d52a6e6 for my_file.py

--- my_file.py
+++ my_file.py
@@ -1,16 +1,3 @@
 print('Hi Git')
 
 print('Hi from another dev')
-
-def: hi_there():
-    print ("hi")
-
-<<<<<<< HEAD
-
-    def: hi_there():
-    print ("Update python function with merge for conflict")
-
-=======
-    def: hi_there():    
-        print ("Update python function with merge for conflict")
->>>>>>> eac84aea3c6cbfe507c42bfc7e2d479cef5d3cca
