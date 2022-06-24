eac84aea3c6cbfe507c42bfc7e2d479cef5d3cca
edoardogunelli <58426812+edoardogunelli@users.noreply.github.com>
2022-06-24 09:59:59 +0200

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -5,11 +5,5 @@ print('Hi from another dev')
 def: hi_there():
     print ("hi")
 
-    def: hi_there():
-    print ("hi")
-<<<<<<< HEAD
-
-print ("hi")
-
-    
-    print ("Update python function with merge for conflict")
+    def: hi_there():    
+        print ("Update python function with merge for conflict")
